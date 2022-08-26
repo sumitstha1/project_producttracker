75e9bb89ff931c299aac8a4451a771fa24094893
sixdgt <edchristian.inventor@gmail.com>
2022-08-26 12:47:51 +0545

    delete function view edit

--- project_producttracker/app_producttracker/urls.py
+++ project_producttracker/app_producttracker/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_index, name='product.index'),
-    # path('editor/', views.product_edit, name='product.edit'),
-    path('create/', views.product_create, name='product.create'),
+    path('products/create/', views.product_create, name='product.create'),
+    path('products/edit/<int:id>/', views.product_edit, name='product.edit'),
+    path('products/update/', views.product_update, name='product.update'),
+    path('products/show/<int:id>/', views.product_show, name='product.show'),
+    path('products/delete/<int:id>/', views.product_delete, name='product.delete'),
 ]
--- project_producttracker/app_producttracker/views.py
+++ project_producttracker/app_producttracker/views.py
@@ -1,6 +1,6 @@
+from multiprocessing import context
 from re import template
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from app_producttracker.forms import ProductCreateForm
 from .models import Product
 
@@ -10,6 +10,42 @@ def product_index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, template, context)
+
+def product_show(request, id):
+    product = Product.objects.get(id=id)
+    template = 'products/show.html'
+    context = {"product": product}
+    return render(request, template, context)
+
+def product_edit(request, id):
+    product = Product.objects.get(id=id)
+    template = 'products/edit.html'
+    context = {"product": product}
+    return render(request, template, context)
+
+def product_update(request):
+    
+    if request.method == 'POST':
+        product = Product.objects.get(id=request.POST.get('id'))
+
+        product.title = request.POST.get('title')
+        product.desc = request.POST.get('desc')
+        product.price = request.POST.get('price')
+        product.quantity = request.POST.get('quantity')
+        product.category = request.POST.get('category')
+        product.user_id = 1
+
+        product.save()
+        return redirect("product.index")
+    return redirect("product.index")
+
+def product_delete(request, id):
+    product = Product.objects.get(id=id)
+    product.delete()
+
+    return redirect('product.index')
+
+
 
 def product_create(request):
     create_form = ProductCreateForm()
